46b3c5b409eba307e7da29a8d49be6857d06f7a6
Loktrbl <conca8048@gmail.com>
2022-03-24 09:16:24 +0700

    DemoGitHup

---
+++ 2001190655_NguyenThanhLoc_PTPM_T1_TL/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _2001190655_NguyenThanhLoc_PTPM_T1_TL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlCheckBoxList = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pnlCheckBoxList.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlCheckBoxList
+            // 
+            this.pnlCheckBoxList.BackColor = System.Drawing.Color.White;
+            this.pnlCheckBoxList.Controls.Add(this.progressBar1);
+            this.pnlCheckBoxList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCheckBoxList.Location = new System.Drawing.Point(0, 0);
+            this.pnlCheckBoxList.Name = "pnlCheckBoxList";
+            this.pnlCheckBoxList.Size = new System.Drawing.Size(608, 420);
+            this.pnlCheckBoxList.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 364);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(480, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 420);
+            this.Controls.Add(this.pnlCheckBoxList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlCheckBoxList.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCheckBoxList;
+        private System.Windows.Forms.ProgressBar progressBar1;
+
+    }
+}
+
---
+++ 2001190655_NguyenThanhLoc_PTPM_T1_TL/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2001190655_NguyenThanhLoc_PTPM_T1_TL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //test();
+            //textbox();
+            caro();
+        }
+
+        public void test()
+        {
+            string[] foods = { "Grain", "Bread", "Beans", "Eggs", "Chicken", "Milk", "Fruit", "Vegetables", "Pasta", "Rice", "Fish", "Beef" };
+            int topPosition = 10;
+            foreach (string food in foods)
+            {
+                CheckBox checkbox = new CheckBox();
+                checkbox.Left = 10;
+                checkbox.Top = topPosition;
+                topPosition += 30;
+                checkbox.Text = food;
+                pnlCheckBoxList.Controls.Add(checkbox);
+            }
+        }
+        public void textbox()
+        {
+            string[] texts = { "textBox1", "textBox2", "textBox3", "textBox4" };
+            int topPosition = 10;
+            foreach (string text in texts)
+            {
+                TextBox textBox = new TextBox();
+                textBox.Size = new Size(300, 20);
+                textBox.Left = 10;
+                textBox.Top = topPosition;
+                topPosition += 30;
+                textBox.Text = text;
+                pnlCheckBoxList.Controls.Add(textBox);
+            }
+        }
+
+        public void caro()
+        {
+
+           
+            int topPosition = 20;
+            int leftPosition = 20;
+            progressBar1.Maximum = 10;
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread.Sleep(500);
+                progressBar1.Value = i;
+              
+                for (int j = 0; j <= 10; j++)
+                {
+                    Button nut = new Button();
+                    nut.BackColor = Color.LightGray;
+                    nut.Size = new Size(30, 30);
+                    nut.Top = topPosition;
+                    nut.Left = leftPosition;
+
+                    leftPosition += 30;
+
+                    pnlCheckBoxList.Controls.Add(nut);
+                    
+                }
+
+                topPosition += 30;
+                leftPosition = 20;
+            }
+
+           
+
+        }
+
+    }
+}
